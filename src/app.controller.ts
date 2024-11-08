cdb309f29039edbdd4f8c4c55cba1172cb2295d5
Berlin V <berlin@surfboard.se>
2024-11-08 17:44:53 +0530

    feat():added interface for the password manager

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { frameResponse } from './utils/response.frame';
 
 @Controller()
 export class AppController {
+  private logger: Logger = new Logger('PASSWORD-MANAGER');
+
   constructor(private readonly appService: AppService) {}
+
+  @Post('savePassword')
+  savePassword(@Body() payload) {
+    try {
+      this.logger.log(
+        `Handling request to save password with payload, ${JSON.stringify(
+          payload,
+        )}`,
+      );
+
+      return frameResponse('SUCCESS', 'Password Saved Successfully');
+    } catch (err) {
+      this.logger.debug(
+        `Save password request failed with an error message,${err.message}`,
+      );
+    }
+  }
 
   @Get()
   getHello(): string {
